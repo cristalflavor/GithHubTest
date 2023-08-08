ae04c93a2c092217df4cdba8beb79163415c013b
cristalflavor <ivansdmonte@hotmail.com>
2023-08-08 16:51:36 -0300

    Create DiMonte.java
    Creé una clase

---
+++ GIt_1/src/git_1/DiMonte.java
@@ -0,0 +1,16 @@
+package git_1;
+
+public class DiMonte {
+    private int edad;
+    
+    public DiMonte(int edad){
+        this.edad = edad;
+    }
+    
+    public void setEdad(int edad){
+        this.edad = edad;
+    }
+    public int getEdad(){
+        return this.edad;
+    }
+}
