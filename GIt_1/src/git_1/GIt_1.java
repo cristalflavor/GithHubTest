2535fe856d5dc805b83dc3fb2eb1bd4d8b3189a7
cristalflavor <ivansdmonte@hotmail.com>
2023-08-08 16:45:48 -0300

    Update GIt_1.java
    Arreglado los conflictos

--- GIt_1/src/git_1/GIt_1.java
+++ GIt_1/src/git_1/GIt_1.java
@@ -4,21 +4,15 @@ public class GIt_1 {
 
     public static void main(String[] args) {
         System.out.println("Hola!!");
-<<<<<<< HEAD
+
         System.out.println("a ver, estamos probando la clase 2");
-        
-        
-=======
-        
 
         System.out.println("Todo bien!");
-
-        
+   
         System.out.println("Q tal");
         
         System.out.println("Por aca todo tranqui");
 
->>>>>>> bc7ead60d08349fda70d8f5462c30e8e5b3c3d18
     }
       public void saludar() {
          String nombre = null;
