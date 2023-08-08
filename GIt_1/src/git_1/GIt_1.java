14fbbdb5d2c2cea1412fa25d264eea792c6eff68
cristalflavor <ivansdmonte@hotmail.com>
2023-08-08 16:30:28 -0300

    Update GIt_1.java
    Agregué Todo bien

--- GIt_1/src/git_1/GIt_1.java
+++ GIt_1/src/git_1/GIt_1.java
@@ -1,10 +1,11 @@
-
 package git_1;
 
 public class GIt_1 {
 
     public static void main(String[] args) {
         System.out.println("Hola!!");
+        
+        System.out.println("Todo bien!");
     }
     
 }
