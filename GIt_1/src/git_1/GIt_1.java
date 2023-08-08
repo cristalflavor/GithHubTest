8b7d6dda9fd9aa226f712579366d98daa3cf0741
AmeliaGaddi <pecky991@gmail.com>
2023-08-08 16:30:51 -0300

    Update GIt_1.java
    que mareo esto, agregue otro sout

--- GIt_1/src/git_1/GIt_1.java
+++ GIt_1/src/git_1/GIt_1.java
@@ -5,6 +5,9 @@ public class GIt_1 {
 
     public static void main(String[] args) {
         System.out.println("Hola!!");
+        System.out.println("a ver, estamos probando la clase 2");
+        
+        
     }
     
 }
