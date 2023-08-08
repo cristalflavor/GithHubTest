f50be298612a2c59988a3c59d2e4da5588dbc96a
AmeliaGaddi <pecky991@gmail.com>
2023-08-08 16:51:15 -0300

    Update GIt_1.java

--- GIt_1/src/git_1/GIt_1.java
+++ GIt_1/src/git_1/GIt_1.java
@@ -4,11 +4,9 @@ public class GIt_1 {
 
     public static void main(String[] args) {
         System.out.println("Hola!!");
-<<<<<<< HEAD
         System.out.println("a ver, estamos probando la clase 2");
         
         
-=======
         
 
         System.out.println("Todo bien!");
@@ -18,7 +16,6 @@ public class GIt_1 {
         
         System.out.println("Por aca todo tranqui");
 
->>>>>>> bc7ead60d08349fda70d8f5462c30e8e5b3c3d18
     }
       public void saludar() {
          String nombre = null;
